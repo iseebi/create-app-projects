f1d1facd2ab73715edfdb412e51483926574c5a9
Nobuhiro Ito <iseebi@iseteki.net>
2026-03-14 22:26:40 +0900

    add definitions

--- src/templates.ts
+++ src/templates.ts
@@ -6,6 +6,16 @@ const templates: Template[] = [
     title: "React Redux App",
     description: "A React Redux application with MUI"
   },
+  {
+    value: "cli",
+    title: "CLI application",
+    description: "A command-line interface application using Node.js"
+  },
+  {
+    value: "cloud-run-functions",
+    title: "Cloud Run Functions",
+    description: "A Google Cloud Run Functions application using Node.js"
+  }
 ];
 
 export default templates;
